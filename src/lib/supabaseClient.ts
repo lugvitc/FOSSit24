fb5c4e8f429f8b13a486d34c15f4b28a517d9eb1
Shoubhit Dash <shoubhit2005@gmail.com>
2024-03-27 12:23:41 +0530

    use constant export

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const PROJECT_URL = "https://snytdulgbjatdmtotpmk.supabase.co";
+const PROJECT_URL = 'https://snytdulgbjatdmtotpmk.supabase.co';
 const ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNueXRkdWxnYmphdGRtdG90cG1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5NzcwOTksImV4cCI6MjAyNTU1MzA5OX0.gBKmnpJAqnxkPHx9ARvbJLXmorcBwmdIRbkuNduebAM";
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNueXRkdWxnYmphdGRtdG90cG1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5NzcwOTksImV4cCI6MjAyNTU1MzA5OX0.gBKmnpJAqnxkPHx9ARvbJLXmorcBwmdIRbkuNduebAM';
 
-const supabase = createClient(PROJECT_URL, ANON_KEY);
-export default supabase;
+export const supabase = createClient(PROJECT_URL, ANON_KEY);
