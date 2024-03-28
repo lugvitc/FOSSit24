bcc3bd1e3bc71a56a9586921b5473a6751d5fecb
mradigen <55953083+mradigen@users.noreply.github.com>
2024-03-28 14:01:19 +0530

    deploy: Add BASE_PATH provision for css styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
+	import { base } from '$app/paths';
 </script>
+
+<svelte:head>
+	<link rel="stylesheet" href="{base}/assets/robit/font.css" />
+</svelte:head>
 
 <Navbar />
 <slot />
