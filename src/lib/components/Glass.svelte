a5707c492e4d2844f22a5de5195a8c7ff0678bb8
mradigen <55953083+mradigen@users.noreply.github.com>
2024-04-06 18:50:05 +0530

    add: Registration page design changes

--- src/lib/components/Glass.svelte
+++ src/lib/components/Glass.svelte
@@ -1,5 +1,5 @@
 <div
-	class="glass relative w-full overflow-hidden rounded-3xl border-[1px] border-zinc-500 px-6 py-6 text-zinc-400 xl:px-12 xl:py-16"
+	class="glass relative w-full overflow-hidden rounded-3xl border-[1px] border-zinc-500 px-6 py-12 text-zinc-400 backdrop-blur-xl xl:px-12 xl:py-16"
 >
 	<slot />
 </div>
