8c5bcb45463093f0de4e9edd5e10332547b492cc
KreativeThinker <sagely2005eagle@gmail.com>
2024-04-06 18:25:11 +0530

    Mobile view menu

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,12 +5,14 @@
 	import Button from './Button.svelte';
 	import { base } from '$app/paths';
 
+	let isOpen = false;
+
 	async function logout() {
 		// logout function
 	}
 
 	async function signup() {
-		// sign up function
+		// sign up function collapse navbar-collapse
 	}
 
 	async function login() {
@@ -29,12 +31,16 @@
 			$user = user_data;
 		}
 	});
+
+	function toggleMenu() {
+		isOpen = !isOpen;
+	}
 </script>
 
 <div class="relative m-auto mt-12 flex w-full max-w-6xl justify-between px-8 xl:px-0">
 	<img
 		src="{base}/assets/Asset 2.png"
-		class="gradient absolute left-[-500px] top-[-700px] z-[-100] h-[1000px] w-[1000px] rotate-[2700deg] xl:left-[-700px] xl:top-[-700px]"
+		class="absolute left-[-700px] top-[-700px] z-[-100] h-[1000px] w-[1000px]"
 		alt="Gradient background"
 	/>
 	<div class="flex items-center space-x-12">
@@ -57,5 +63,32 @@
 			<Button href="{base}/" primary>Signup</Button>
 		{/if}
 	</div>
-	<p class="block xl:hidden">Menu</p>
+	<div class="relative flex items-center justify-center xl:hidden">
+		<button class="block h-fit" on:click={toggleMenu}>
+			<div class="space-y-2">
+				<span class="block h-0.5 w-6 bg-zinc-400"></span>
+				<span class="block h-0.5 w-6 bg-zinc-400"></span>
+				<span class="block h-0.5 w-6 bg-zinc-400"></span>
+			</div>
+		</button>
+	</div>
+</div>
+<div
+	class="absolute right-8 z-10 flex w-56 flex-col justify-center rounded-2xl bg-black xl:hidden"
+	style="display: {isOpen ? 'block' : 'none'};"
+>
+	<div class="space-y-4 px-8 py-8 xl:hidden">
+		<a href="{base}/" class="block">Leaderboard</a>
+		<a href="{base}/" class="block">Projects</a>
+		<a href="{base}/" class="block">Dashboard</a>
+		<div class="flex flex-col space-y-4 xl:hidden">
+			{#if $user.id}
+				<a href="{base}/">Logout</a>
+				<a href="{base}/" class="... truncate">{$user.name}</a>
+			{:else}
+				<a href="{base}/">Login</a>
+				<a href="{base}/">Signup</a>
+			{/if}
+		</div>
+	</div>
 </div>
