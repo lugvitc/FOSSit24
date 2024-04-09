4e31d66e64ff3b13992762d9043b5b24de264f57
KreativeThinker <sagely2005eagle@gmail.com>
2024-04-09 15:56:40 +0530

    Moved  to

---
+++ src/routes/(protected)/ideas/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Glass from '$lib/components/Glass.svelte';
+	import Card from '$lib/components/Card.svelte';
+	import Section from '$lib/components/Section.svelte';
+	import Input from '$lib/components/Input.svelte';
+	import { user, auth_user } from '$lib/stores';
+	import { onMount } from 'svelte';
+	import supabase from '$lib/supabase';
+
+	let ideas: any = [];
+	onMount(async () => {
+		let { data, error } = await supabase.from('ideas').select('*');
+		ideas = data;
+	});
+</script>
+
+<Section>
+	<div class="grid grid-cols-1 gap-x-4 gap-y-4 sm:grid-cols-2 lg:grid-cols-3">
+		{#each ideas as idea}
+			<Card {idea} />
+		{/each}
+	</div>
+</Section>
