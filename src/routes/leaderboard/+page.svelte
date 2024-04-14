b880e38f5ac53852912dfd6a548dd6c964fd7d3a
leo <166975150+TheLostLeo@users.noreply.github.com>
2024-04-14 18:02:26 +0530

    Update +page.svelte

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -17,7 +17,9 @@
 		// loading = true;
 		const { data, error } = await supabase.from('leaderboard').select('name, points');
 		if (error) console.log('Error Fetching Teams:', error.message);
-		if (data.length) leaderboard = data;
+		if (data.length) {
+			leaderboard = data.sort((a, b) => b.points - a.points); // Sort leaderboard by points
+		}
 		loading = false;
 	}
 
