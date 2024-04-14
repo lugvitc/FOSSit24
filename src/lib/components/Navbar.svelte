eca43f060a9081ae67baad1f9892acc568965e24
mradigen <55953083+mradigen@users.noreply.github.com>
2024-04-14 12:57:25 +0530

    Let's get started

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -59,47 +59,47 @@
 			</a>
 			<a href="https://lugvitc.net">LUGVITC</a>
 		</div>
-		<!-- <div class="hidden items-center space-x-12 xl:flex"> -->
-		<!-- 	<a href="{base}/leaderboard">Leaderboard</a> -->
-		<!-- 	<a href="{base}/projects">Projects</a> -->
-		<!-- 	<a href="{base}/ideas">Ideas</a> -->
-		<!-- </div> -->
+		<div class="hidden items-center space-x-12 xl:flex">
+			<a href="{base}/leaderboard">Leaderboard</a>
+			<a href="{base}/projects">Projects</a>
+			<a href="{base}/ideas">Ideas</a>
+		</div>
 	</div>
-	<!-- <div class="hidden items-center space-x-4 xl:flex"> -->
-	<!-- 	{#if $auth_user.id} -->
-	<!-- 		<button on:click={logout}>Logout</button> -->
-	<!-- 		<a href="{base}/dashboard" class="... max-w-72 truncate" data-primary -->
-	<!-- 			>@{$auth_user.user_metadata.user_name}</a -->
-	<!-- 		> -->
-	<!-- 	{:else} -->
-	<!-- 		<button data-primary on:click={login}>Login</button> -->
-	<!-- 	{/if} -->
-	<!-- </div> -->
-	<!-- <div class="relative flex items-center justify-center xl:hidden"> -->
-	<!-- 	<button class="block h-fit border-0 !p-0" on:click={toggleMenu}> -->
-	<!-- 		<div class="space-y-2"> -->
-	<!-- 			<span class="block h-0.5 w-6 bg-zinc-400"></span> -->
-	<!-- 			<span class="block h-0.5 w-6 bg-zinc-400"></span> -->
-	<!-- 			<span class="block h-0.5 w-6 bg-zinc-400"></span> -->
-	<!-- 		</div> -->
-	<!-- 	</button> -->
-	<!-- </div> -->
+	<div class="hidden items-center space-x-4 xl:flex">
+		{#if $auth_user.id}
+			<button on:click={logout}>Logout</button>
+			<a href="{base}/dashboard" class="... max-w-72 truncate" data-primary
+				>@{$auth_user.user_metadata.user_name}</a
+			>
+		{:else}
+			<button data-primary on:click={login}>Login</button>
+		{/if}
+	</div>
+	<div class="relative flex items-center justify-center xl:hidden">
+		<button class="block h-fit border-0 !p-0" on:click={toggleMenu}>
+			<div class="space-y-2">
+				<span class="block h-0.5 w-6 bg-zinc-400"></span>
+				<span class="block h-0.5 w-6 bg-zinc-400"></span>
+				<span class="block h-0.5 w-6 bg-zinc-400"></span>
+			</div>
+		</button>
+	</div>
 </div>
-<!-- <div -->
-<!-- 	class="absolute right-8 z-10 flex w-56 flex-col justify-center rounded-2xl bg-black xl:hidden" -->
-<!-- 	style="display: {isOpen ? 'block' : 'none'};" -->
-<!-- > -->
-<!-- 	<div class="space-y-4 px-8 py-8 xl:hidden"> -->
-<!-- 		<a href="{base}/leaderboard" class="block">Leaderboard</a> -->
-<!-- 		<a href="{base}/projects" class="block">Projects</a> -->
-<!-- 		<a href="{base}/ideas" class="block">Ideas</a> -->
-<!-- 		<div class="flex flex-col space-y-4 xl:hidden"> -->
-<!-- 			{#if $auth_user.id} -->
-<!-- 				<a on:click={logout} class="cursor-pointer">Logout</a> -->
-<!-- 				<a href="{base}/dashboard" class="... truncate">@{$auth_user.user_metadata.user_name}</a> -->
-<!-- 			{:else} -->
-<!-- 				<a on:click={login} class="cursor-pointer">Login</a> -->
-<!-- 			{/if} -->
-<!-- 		</div> -->
-<!-- 	</div> -->
-<!-- </div> -->
+<div
+	class="absolute right-8 z-10 flex w-56 flex-col justify-center rounded-2xl bg-black xl:hidden"
+	style="display: {isOpen ? 'block' : 'none'};"
+>
+	<div class="space-y-4 px-8 py-8 xl:hidden">
+		<a href="{base}/leaderboard" class="block">Leaderboard</a>
+		<a href="{base}/projects" class="block">Projects</a>
+		<a href="{base}/ideas" class="block">Ideas</a>
+		<div class="flex flex-col space-y-4 xl:hidden">
+			{#if $auth_user.id}
+				<a on:click={logout} class="cursor-pointer">Logout</a>
+				<a href="{base}/dashboard" class="... truncate">@{$auth_user.user_metadata.user_name}</a>
+			{:else}
+				<a on:click={login} class="cursor-pointer">Login</a>
+			{/if}
+		</div>
+	</div>
+</div>
